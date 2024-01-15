685082c1c3f4c6378abdcff1f99ab967788e1d00
snmartinez <surymtz17@gmail.com>
2024-01-15 10:57:42 -0600

    el update de roles tiene un error

---
+++ LoginBlazor/LoginShared/Entity/UserEntity.cs
@@ -0,0 +1,26 @@
+﻿namespace LoginShared.Entity;
+
+public record UserEntity(
+    int Id,
+    string Name,
+    string LastName,
+    string Department,
+    string Email,
+    string Password,
+    string Puesto
+)
+{
+   
+    // public object Username { get; set; }
+    
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public string LastName { get; init; }
+    public string Department { get; init; }
+    public string Email { get; init; }
+    public string Password { get; init; }
+    
+    public string Code => Email;
+    public string FullName => $"{Name} {LastName}";
+    
+};
