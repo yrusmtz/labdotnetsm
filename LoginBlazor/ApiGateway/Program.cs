a712ec44ba7a06c5195664fce1f759500e43674f
helmutpadilla <helmutpadilla@gmail.com>
2024-01-23 08:35:21 -0600

    Adding Api gateway with ocelot

---
+++ LoginBlazor/ApiGateway/Program.cs
@@ -0,0 +1,27 @@
+using Common.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace ApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                        .ConfigureAppConfiguration((hostingContext, config) =>
+                        {
+                            config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+                        })
+                        .ConfigureWebHostDefaults(webBuilder =>
+                        {
+                            webBuilder.UseStartup<Startup>();
+                        });        
+    }
+}
